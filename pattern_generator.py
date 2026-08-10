b093e6bd549f1a3b4ef92259f38f43424a6291d9
Gillian Dodge <gillian@fenderagency.onmicrosoft.com>
2026-08-10 14:41:15 -0400

    adding initial program structure, updated sweater sizing

---
+++ pattern_generator.py
@@ -0,0 +1,63 @@
+#goal: create a program that generates a sweater pattern from a pre-set pattern
+#tailor the pattern to specific gauge and size
+
+#outline sweater pattern options
+#yoke sweater
+
+#raglan sweater
+
+
+#size options (display size measurements)
+#all sizes in inches
+#extra-small
+xs_bust = 37.5
+xs_length = 18.5
+xs_arm_circ = 
+xs_sleeve_length = 
+
+#small
+s_bust = 40.25
+s_length = 19.0
+s_arm_circ = 
+s_sleeve_length = 
+
+#medium
+m_bust = 42.5
+m_length = 19.75
+m_arm_circ = 
+m_sleeve_length = 
+
+#large
+l_bust = 45.25
+l_length = 20.5
+l_arm_circ = 
+l_sleeve_length = 
+
+#extra-large
+xl_bust = 49.25
+xl_length = 21.25
+xl_arm_circ = 
+xl_sleeve_length = 
+
+#xxl
+xxl_bust = 52.0
+xxl_length = 21.75
+xxl_arm_circ = 
+xxl_sleeve_length = 
+
+
+#gauge calculator
+#gauge = 4x4 in
+# sts x rows
+#needle size
+
+#ask needle size (account for US sizing or mm sizing)
+#ask how many sts for the gauge
+#ask how many rows are needed for the gauge
+
+#input for size
+
+#input for pattern selection
+
+
+#save option for textfile of pattern
